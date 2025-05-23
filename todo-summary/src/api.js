6cc2f9e89df79ee25462776a3a38579aee7af092
Nanditha2625 <nanditha2625@gmail.com>
2025-05-23 13:06:27 +0530

    Delete todo-summary directory

--- todo-summary/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = "http://localhost:8080";
-
-export const getTodos = () => axios.get(`${API_BASE_URL}/todos`);
-export const addTodo = (todo) => axios.post(`${API_BASE_URL}/todos`, todo);
-export const deleteTodo = (id) => axios.delete(`${API_BASE_URL}/todos/${id}`);
-export const summarizeTodos = () => axios.post(`${API_BASE_URL}/summarize`);
